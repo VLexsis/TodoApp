e08eea6c21f32c9b02d56488136ddcfea1eba404
Aleksey <steam_qw@mail.ru>
2025-01-13 16:26:33 +0300

    Началась работа над функционалом таймера для задач

---
+++ .history/src/components/NewTaskForm/NewTaskForm_20250113161841.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'y
+
+import PropTypes from 'prop-types'
+
+export default class NewTaskForm extends Component {
+    state = {
+        description: '',
+    }
+
+    onTaskChange = (event) => {
+        this.setState({
+            description: event.target.value,
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        this.props.onAddTask(this.state.description)
+        this.setState({
+            description: '',
+        })
+    }
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    className="new-todo"
+                    placeholder="What needs to be done?"
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onTaskChange}
+                />
+            </form>
+        )
+    }
+}
+NewTaskForm.propTypes = {
+    onAddTask: PropTypes.func.isRequired,
+}
+NewTaskForm.defaultProps = {
+    onAddTask: () => {},
+}
